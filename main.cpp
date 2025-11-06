930ceabbdd4065c1235c651f34e4cfe513141f83
Alisher2207 <250087@newuu.uz>
2025-11-06 12:46:58 +0500

    main till 67
    Commented out several functions and the main function for future reference.

--- main.cpp
+++ main.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
+#include <array>
+
 using namespace std;
-const int SIZE = 4;
+//const int SIZE = 4;
 
-double sumColumn(const double m[] [SIZE], int rowsize,int columnIndex) {
- double sum = 0;
- for (int i=0;i<rowsize; i++)
-  sum+= m[i] [columnIndex];
- return sum;
- double summajordiagonal(const double m[SIZE][SIZE]) {
-  double sum =0;
-  for (int i=0;i<SIZE; i++) {
-   sum += m[i][i];
-  }
+//double sumColumn(const double m[] [SIZE], int rowsize,int columnIndex) {
+ //double sum = 0;
+ //for (int i=0;i<rowsize; i++)
+  //sum+= m[i] [columnIndex];
+ //return sum;
+ //double summajordiagonal(const double m[SIZE][SIZE]) {
+  //double sum =0;
+  //for (int i=0;i<SIZE; i++) {
+   //sum += m[i][i];
+  //}
 
- }
+ //}
  //problem1
  // const int size = 4;
  //
@@ -54,15 +56,99 @@ double sumColumn(const double m[] [SIZE], int rowsize,int columnIndex) {
  // }
  //
  //Again problem2
- int main() {
-  double m [SIZE][SIZE];
-  for (int i=0;i< SIZE; j++) {
-   for (int j =0 ; j< SIZE ; j++) {
-    cin>> m[i][j];
+ //int main() {
+//  double m [SIZE][SIZE];
+  //for (int i=0;i< SIZE; j++) {
+   //for (int j =0 ; j< SIZE ; j++) {
+    //cin>> m[i][j];
 
-   }
-  }
-  cout<<summajordiagonal(m)<<endl;
-  return 0 ;
- }
-}
+   //}
+  //}
+  //cout<<summajordiagonal(m)<<endl;
+  //return 0 ;
+ //}
+//}
+//problem3
+//
+// const int n=4;
+// void multiplyMatrix(const double a[][n], const double b[][n], const double c[][n]) {
+// for (int i = 0; i < n; i++) {
+// for (int j = 0; j < n; j++) {
+// for (int k = 0; k < n; k++) {
+// c[i][j] += a[i][k] * b[k][j];
+// }
+// }
+// }
+// for(int i = 0; i < n; i++) {
+// for(int j = 0; j < n; j++) {
+// cout<<c[i][j]<<" ";
+// }cout<<endl;
+// }
+//
+// }
+// int main() {
+//  double a[n][n], b[n][n], c[n][n];
+//  cout<<"Enter matrix A:"<<endl;
+//  for(int i = 0; i < n; i++) {
+//   for(int j = 0; j < n; j++) {
+//    cin>>a [i][j];
+//   }
+//  }
+//  for(int i = 0; i < n; i++)
+//   for(int j = 0; j < n; j++) {
+//    cin>>b[i][j];
+//   }
+//
+//  multiplyMatrix(a,b,c);
+// }
+
+
+//problem4
+// int findMax(int mat[][100], int n ,int m) {
+//  int max=mat[0][0];
+//  for(int i=0;i<n;i++) {
+//   for(int j=0;j<m;j++) {
+//    if(mat[i][j]>max) max=mat[i][j];
+//   }
+//  }
+//  return max;
+// }
+// int main() {
+//  int n, m;
+//  cin>>n>>m;
+//  int mat[100][100];
+//  for(int i=0;i<n;i++) {
+//   for(int j=0;j<m;j++) {
+//    cin>>mat[i][j];
+//   }
+//  }
+//  cout<<findMax(mat, n, m)<<endl;
+//  return 0;
+// }
+//problem5
+
+//problem6
+// int main() {
+//  int n,m ;
+//  cout<<"row" ; cin>>n ;
+//
+//  cout<<"column"; cin>>m ;
+//
+//  int matrix[n][m];
+//  for(int i=0;i<n;i++) {
+//   for(int j=0;j<m;j++) {
+//    cin>>matrix[i][j];
+//   }
+//  }
+//  for (int i=0;i<n;i++) {
+//   int sum=0;
+//   for (int j=0;j<m;j++) {
+//    sum+=matrix[i][j];
+//    cout<<matrix[i][j]<<" ";
+//   }cout<<"Average of sum" <<double(sum)/m<<endl;
+//
+//  }
+//  return 0;
+// }0
+
+//problem7
